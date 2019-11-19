b79440898d063454130da20feabdf0ec0c719883
KamikariCat <kamikari@yahoo.com>
2019-11-19 02:10:19 +0200

    auth middleware start...

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -5,6 +5,16 @@ const User = require('./user');
 module.exports = function () {
     const router = new Router();
 
+    router.use((ctx, next) => { // :TODO:
+        if (!ctx.request.headers.hasOwnProperty('token') ||
+        !ctx.request.headers.hasOwnProperty('login')) {
+            ctx.ststus = 401;
+            ctx.body = 'Token or login was not exists';
+        } else {
+            next();
+        }
+    });
+
     for (const route in User) if (User.hasOwnProperty(route)) User[route](router);
 
     return router;
