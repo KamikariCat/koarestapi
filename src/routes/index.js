3bfa473134453d05fe0f4b0265744240170b83d3
KamikariCat <kamikari@yahoo.com>
2019-12-15 15:51:41 +0200

    role route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const MainRouter = require('./main');
 const UserRouter = require('./user');
+const RoleRouter = require('./role');
 
 module.exports = function () {
     this
         .use(MainRouter().routes())
-        .use(UserRouter().routes())
         .use(MainRouter().allowedMethods())
-        .use(UserRouter().allowedMethods());
+
+        .use(UserRouter().routes())
+        .use(UserRouter().allowedMethods())
+
+        .use(RoleRouter().routes())
+        .use(RoleRouter().allowedMethods())
 };
